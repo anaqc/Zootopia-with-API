d8663c9029e9d4d96b63fd09500361206ae1fed9
elisaquispe2011 <elisaquispe2011@gmail.com>
2025-01-07 21:28:32 +0100

    remove unused imports

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,18 +1,5 @@
 import data_fetcher
-import json
 
-
-def load_data(file_path):
-    """ Loads a JSON file """
-    try:
-        with open(file_path, "r", encoding="utf-8") as handle:
-            return json.load(handle)
-    except FileNotFoundError:
-        print(f"Error: File {file_path} not found")
-        raise
-    except json.JSONDecodeError:
-        print(f"Error: Invalid JSON in {file_path}")
-        raise
 
 def read_animals_html(file_path):
     """This function load the animals_template.html file"""
