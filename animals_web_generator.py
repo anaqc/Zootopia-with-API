e828737050503dac543e162a5a89d40c78d2395b
elisaquispe2011 <elisaquispe2011@gmail.com>
2025-01-08 17:47:07 +0100

    modified function get_animal_name_from_user

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -10,18 +10,21 @@ def read_animals_html(file_path):
 def serialize_animal(animal_obj):
     """ This function handle a single animal serialization"""
     # define an empty string
-    output = ""
+    animal_name = animal_obj.get('name')
+    animal_scientific_name = animal_obj['taxonomy'].get('scientific_name')
+    animal_diet = animal_obj['characteristics'].get('diet')
     animal_type = animal_obj["characteristics"].get("type")
     animal_color = animal_obj["characteristics"].get("color")
+    animal_location = animal_obj["locations"][0]
+    output = ""
     # append information to each string
     output += "<li class='cards__item'>\n\t"
-    output += f"<div class='card__title'>{animal_obj.get('name')}</div>\n\t"
+    output += f"<div class='card__title'>{animal_name}</div>\n\t"
     output += "<p class='card__text'>\n\t\t"
     output += "<ul>\n\t\t\t"
-    output += (f"<li><strong>Scientific Name:</strong> "
-               f"{animal_obj['taxonomy'].get('scientific_name')}</li>\n\t\t\t")
-    output += f"<li><strong>Diet:</strong> {animal_obj['characteristics'].get('diet')}</li>\n\t\t\t"
-    output += f"<li><strong>Location:</strong> {animal_obj["locations"][0]}</li>\n\t\t\t"
+    output += f"<li><strong>Scientific Name:</strong>{animal_scientific_name}</li>\n\t\t\t"
+    output += f"<li><strong>Diet:</strong> {animal_diet}</li>\n\t\t\t"
+    output += f"<li><strong>Location:</strong> {animal_location}</li>\n\t\t\t"
     if animal_color is not None:
         output += f"<li><strong>Color:</strong> {animal_color}</li>\n\t\t\t"
     if animal_type is not None:
@@ -31,16 +34,17 @@ def serialize_animal(animal_obj):
     output += "</li>\n"
     return output
 
+
 def write_new_content(file_path, content):
     """ This function write the content to a new file"""
     with open(file_path, "w") as f:
         f.write(content)
 
 
-def animals_data_not_found(name):
+def animals_data_not_found(animal_name):
     """ This function create a new site if the animal searched doesn't exist"""
     output = ""
-    output += "<h2><strong>Error Message:</strong> Oops! Animal Not Found</h2>\n"
+    output += f"<h2><strong>Error Message:</strong> Oops! Animal * {animal_name} * Not Found</h2>\n"
     output += "<li class='cards__item'>\n\t"
     output += f"<div class='card__title'> We couldn't find the animal you're looking for</div>\n\t"
     output += "<p class='card__text'>\n\t\t"
@@ -52,16 +56,20 @@ def animals_data_not_found(name):
     output += "</li>\n"
     return output
 
+
 def get_animal_name_from_user():
+    """ This function ask the user to insert validate animal name """
     while True:
         try:
             animal_name = input("Enter a name of an animal: ")
-            if animal_name == "" or animal_name.isdigit():
+            if animal_name == "" or animal_name.isdigit() or len(animal_name) < 2:
                 print("You entered something that is not a animal name")
                 continue
             return animal_name
         except Exception as e:
             print("You entered something that is not a animal name", e)
+
+
 def main():
     new_file_path = "animals.html"
     animal_name = get_animal_name_from_user()
